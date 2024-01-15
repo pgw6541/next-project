a6a685bc21d5ab4836744c65bfb0e0563b672e36
pgw6541 <pgw6541@gmail.com>
2024-01-15 18:24:59 +0900

    fuelType 삭제

--- redux/slice/selectedCar.ts
+++ redux/slice/selectedCar.ts
@@ -1,16 +1,14 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { Car } from '@/types/types';
+// import { Car } from '@/types/types';
 
 interface SelectedCarState {
   brand: string;
   segment: string;
-  fuelType: string;
 }
 
 const initialState: SelectedCarState = {
   brand: '',
   segment: '',
-  fuelType: '',
 }
 
 export const selectedCar = createSlice({
@@ -23,10 +21,7 @@ export const selectedCar = createSlice({
     setSegment: (state, action: PayloadAction<string>) => {
       state.segment = action.payload;
     },
-    setFuelType: (state, action: PayloadAction<string>) => {
-      state.fuelType = action.payload;
-    }, 
   },
 });
 
-export const { setBrand, setSegment, setFuelType } = selectedCar.actions;
+export const { setBrand, setSegment } = selectedCar.actions;
