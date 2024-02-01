1d9b8de5a2b7a6651b01ead95766a3d95acd623b
pgw6541 <pgw6541@gmail.com>
2024-02-01 23:22:53 +0900

    Update 차량 보러가기 버튼 조건 설정

--- app/search/category.tsx
+++ app/search/category.tsx
@@ -21,30 +21,28 @@ export default function Category() {
   const selectOption = useAppSelector(state => state.selectOption); // 차량선택
   const chooseCar = useAppSelector(state => state.chooseCar);
   const dispatch = useAppDispatch();
-
   // Modal
   const [modalShow, setModalShow] = useState(false);
   const [modalContent, setModalContent] = useState('');
 
-  const [brands, setBrands] = useState<types.Brands[]>()  // 브랜드리스트
-  const [segments, setSegments] = useState<string[]>() // 차급리스트
-
+  const [brandList, setBrandList] = useState<types.Brands[]>()  // 브랜드리스트
+  const [segmentList, setSegmentList] = useState<string[]>() // 차급리스트
+  
   // 브랜드목록, 차급 목록 가져오기(비동기)
   useEffect(() => {
+    setBrandList(brandData)
 
     // segmenct 가져오기
     const seg = Array.from( new Set(carData.map(e => e.segment)));
+    setSegmentList(seg)
+  }, [carData, brandData])
 
-    setBrands(brandData)
-    setSegments(seg)
-  }, [carData])
-
-  // Brand 변경함수
+  // 선택한 Brand로 변경함수
   const selectBrandHandler = (brand: string) => {
     console.log('변경: ' + brand)
     dispatch(setBrand(brand))
   }
-  // Segment 변경함수
+  // 선택한 Segment로 변경함수
   const selectSegmentHandler = (brand: string) => {
     console.log('변경: ' + brand)
     dispatch(setSegment(brand))
@@ -54,13 +52,6 @@ export default function Category() {
   // 아무것도 선택하지않았다면 차량 전체 목록
   const [filterBrand, setFilterBrand] = useState<types.Car[]>(carData);
   const [filterSeg, setFilterSeg] = useState<types.Car[]>(carData);
-  // const [chooseCar, setChooseCar] = useState<Car[]>(carData);
-  
-  useEffect(() => {
-    setFilterBrand(carData)
-    setFilterSeg(carData)
-    dispatch(setChoose(carData))
-  }, [carData, dispatch])
 
   useEffect(() => {
     if(selectOption.brand!=''){
@@ -80,20 +71,15 @@ export default function Category() {
     }
   }, [carData, selectOption.segment]) // 변경함수에서 변경감지
 
-
+  // 선택한 옵션 store.chooseCar에 할당
   useEffect(() => {
-    if(selectOption.brand!='' && selectOption.segment===''){
-      dispatch(setChoose(filterBrand))
-    }
-    if(selectOption.brand==='' && selectOption.segment!=''){
-      dispatch(setChoose(filterSeg))
-    }
     if(selectOption.brand!='' && selectOption.segment!=''){
       dispatch(setChoose(filterBrand.filter( e => e.segment === selectOption.segment)));
+    } else if(selectOption.brand===''){
+      dispatch(setChoose(filterSeg))
+    } else if(selectOption.segment===''){
+      dispatch(setChoose(filterBrand))
     }
-    // 종속성 경고off 주석
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    
   }, [carData, filterBrand, filterSeg, dispatch, selectOption.brand, selectOption.segment])
   
   // 모달 열고 닫기
@@ -101,13 +87,21 @@ export default function Category() {
     setModalContent(content);
     setModalShow(true);
   }
+  // 초기화
+  const setClear = () => {
+    dispatch(setBrand(''))
+    dispatch(setSegment(''))
+  }
 
   return(
     <div>
+
+
+
       {/* Modal */}
       <Modal show={modalShow} hide={!modalShow} >
         { modalContent === 'brand' &&
-          brands?.map((brand, i)=>(
+          brandList?.map((brand, i)=>(
             <div className={search.modal_img} key={i} onClick={()=>{selectBrandHandler(brand.name.kr); setModalShow(false)}} >
               <Image
               src={`https://raw.githubusercontent.com/pgw6541/CarSite/main/src/images/${brand.imgUrl}.png`}
@@ -119,7 +113,7 @@ export default function Category() {
           ))
         }
         { modalContent === 'segment' &&
-          segments?.map((segment, i)=>(
+          segmentList?.map((segment, i)=>(
             <div className={search.modal_img} key={i} onClick={()=>{selectSegmentHandler(segment); setModalShow(false)}}>
               <p>{segment}</p>
               {/* <Image
@@ -135,12 +129,13 @@ export default function Category() {
             <span className={search.close} onClick={()=>{setModalShow(false)}}>&times;</span>
         </div>
       </Modal>
+
       {/* 검색창 */}
       <div className={`${search.searchBar} ${search.container} `}>
         <input id='search-input' type="text" placeholder="검색어를 입력하시오" />
       </div>
-      {/* 옵션 카테고리 */}
 
+      {/* 옵션 카테고리 */}
       <ul className={search.container}>
         {/* 제조사 Brand */}
         <li className={search.li} >
@@ -160,11 +155,26 @@ export default function Category() {
 
       {/* Category 하단버튼 */}
       <div className={search.bottomBtns}>
-        <div className={`${search.btn}, ${search.reset}`}>초기화</div>
-        <Link href={'/list'} className={`${search.btn}, ${search.complete}`}>{chooseCar?.length}개의 차량 보러가기</Link>
+        <div onClick={()=>{setClear()}} className={`${search.btn}, ${search.reset}`}>초기화</div>
+        
+        {/* 옵션 선택에 따라 상이한 버튼 */}
+        {
+          selectOption.brand==='' && selectOption.segment===''
+          ?
+          <div>옵션을 선택해주세요</div>
+          :
+          chooseCar.length===0
+          ?
+          <div>선택한 옵션에 차량이 없습니다</div>
+          :
+          <Link href={'/list'} className={`${search.btn}, ${search.complete}`}>
+            {chooseCar?.length}개의 차량 보러가기
+          </Link>
+        }
       </div>
 
-      
+
+
     </div>
   )
 }
