2906ed1615a7824bedecda2f4d37b2dc7ef5e129
pgw6541 <pgw6541@gmail.com>
2024-02-06 16:50:20 +0900

    main page 검색 페이지 링크

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
   return (
     <div>
       {/* 검색링크 */}
-      <div className={main.link_container}>
-        <Link href={'/search'}>검색</Link>
-      </div>
+      <section className={main.link_container}>
+        <Link href={'/search'}><p>검색</p></Link>
+      </section>
     </div>
   )
 }
