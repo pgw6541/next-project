37f61d5f7018b907d28198143d2570641d8bcd03
pgw6541 <pgw6541@gmail.com>
2024-02-07 00:11:33 +0900

    list_info section 토글

--- app/list/carList.tsx
+++ app/list/carList.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import list from './list.module.scss'
 import Link from "next/link";
 import Image from "next/image";
@@ -8,12 +8,18 @@ import Image from "next/image";
 import { useAppSelector, useAppDispatch } from "@/store/hook"
 import { setChoose } from '@/store/slice/chooseCar';
 import { useCarData } from "@/hooks/useData";
+import * as types from '@/types/types'
+
+interface showDetailCars extends types.Car {
+  showDetail: boolean;
+}
 
 export default function CarList(){
   const carData = useCarData();
   const dispatch = useAppDispatch();
   const [selectBrand, selectSeg] = [useAppSelector(state => state.selectOption.brand), useAppSelector(state => state.selectOption.segment)]
   const chooseCar = useAppSelector( state => state.chooseCar)
+  const [cars, setCars] = useState<showDetailCars[]>()
    
   useEffect(()=>{
     if(chooseCar.length===0){
@@ -23,26 +29,72 @@ export default function CarList(){
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [carData])
 
+  useEffect(()=>{
+    const addShowDetail = chooseCar.map( car => ({
+      ...car,
+      showDetail: false,
+    }))
+    // console.log(addShowDetail)
+    setCars(addShowDetail)
+  }, [carData, chooseCar])
+
+  const handleDetailSec = (id: number) =>{
+    // const thisCar = cars?.filter( e =>  e.id === id).map( e => e.showDetail = !e.showDetail)
+    const changeShow = cars?.map( car => car.id === id ? { ...car, showDetail: !car.showDetail} : car);
+    setCars(changeShow)
+    
+  }
+
   return (
     <div className="container">
       {/* Car Section */}
-      <div className={`${list.section}`}>
+      <div className={`${list.car_container}`}>
         {
-          chooseCar.map((car, i)=> (
-            <Link href={`/detail/${car.id}`} className={`${list.box}`} key={i}>
-              <Image 
-                className={`${list.img}`} 
-                src={`https://raw.githubusercontent.com/pgw6541/CarSite/main/src/images/${car.imgUrl}.png`}
-                alt={car.name.en}
-                width={360}
-                height={153}
-              />
-              <div>
-                <p>{car.name.kr}</p>
-                <p>가격 : {car.price.min}~{car.price.max}</p>
+          cars ?
+          cars.map((car, i)=> (
+            <div key={i}>
+              {/* Car */}
+              <div className={list.car_section}>
+                <Link className={list.img_ctn} href={`/detail/${car.id}`} >
+                  {/* 차량 이미지 */}
+                  <Image
+                    className={list.img}
+                    src={`https://raw.githubusercontent.com/pgw6541/CarSite/main/src/images/${car.imgUrl}.png`}
+                    alt={car.name.en}
+                    width={360}
+                    height={153}
+                  />
+                </Link>
+                <div className={list.article}>
+                  {/* 차량 이름 */}
+                  <Link className={list.name} href={`/detail/${car.id}`}>
+                    <p>{car.brand.kr}&nbsp;</p>
+                    <p>{car.name.kr}</p>
+                  </Link>
+
+                  {/* 상세정보 버튼*/}
+                  <div onClick={()=>{handleDetailSec(car.id)}} className={list.InfoBtn}>상세보기</div>
+                </div>
               </div>
-            </Link>
+              {/* Info */}
+              <div className={ car.showDetail ? `${list.info_section} ${list.show}` : `${list.info_section} ${list.hide}` } >
+                {/* info_article 1 */}
+                <div>
+                  <p>옵션1-1</p>
+                  <p>옵션1-2</p>
+                  <p>옵션1-3</p>
+                </div>
+                {/* info_article 2 */}
+                <div>
+                  <p>옵션2-1</p>
+                  <p>옵션2-2</p>
+                  <p>옵션2-3</p>
+                </div>
+              </div>
+            </div>
           ))
+          :
+          <div>선택한 값이 없거나 잘못된 경로로 접근하였습니다.</div>
         }
       </div>
     </div>
