8d7e8e5b89443375517561fe2dcc276a904c152f
pgw6541 <pgw6541@gmail.com>
2024-03-05 19:42:45 +0900

    footer 숨기기(usePathname)

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,9 +1,17 @@
-import style from './footer.module.scss';
+'use client'
+
+import { usePathname } from 'next/navigation';
+import footer from './footer.module.scss';
 
 export default function Footer() {
+
+  const pathname = usePathname();
+
+  if(pathname === '/search'){
+    return null;
+  }
+
   return(
-    <footer>
-      
-    </footer>
+    <footer></footer>
   )
 }
