7e7cb8e4d09a30cb71940e4c1e9f4363aefe87de
pgw6541 <pgw6541@gmail.com>
2024-03-06 18:04:57 +0900

    getGistData 정리

--- pages/api/getGistData.ts
+++ pages/api/getGistData.ts
@@ -1,9 +1,5 @@
 import { Car } from '@/types/types'
 
-
-// gits에서 자동차 데이터 가져오기
-// const gitsUrl = 'https://gist.githubusercontent.com/pgw6541/3bec1be58457d14aab3e04fea8434458/raw/489ee75632f5d987cbc6e423bacce6ec8af481d6/CarData.json'
-
 async function fetchData(gitsUrl: string): Promise<Car[]>{
   try {
     const res = await fetch(gitsUrl);
@@ -18,9 +14,5 @@ async function fetchData(gitsUrl: string): Promise<Car[]>{
     throw error
   }
 }
-
-
-// MongoDB
-
 
 export { fetchData };
