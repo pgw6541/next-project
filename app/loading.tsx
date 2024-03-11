c99de2e15faa2d07a10f0610124e69ea6fe09ec8
pgw6541 <pgw6541@gmail.com>
2024-03-11 23:57:09 +0900

    loading 비활성화

--- app/loading.tsx
+++ app/loading.tsx
@@ -5,7 +5,7 @@ import loading from './loading.module.scss'
 export default function Loading(){
   return (
     <div className={loading.container}>
-      <div className={loading.circle}></div>
+      {/* <div className={loading.circle}></div> */}
     </div>
   )
 }
