f74397426954221206aa019ec0f62a69b7b7e2b9
pgw6541 <pgw6541@gmail.com>
2024-03-12 16:39:53 +0900

    Delete loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import main from "./main.module.scss"
 
 
 export default function Home() {
@@ -7,7 +6,7 @@ export default function Home() {
   return (
     <div>
       {/* 검색링크 */}
-      <section className={main.link_container}>
+      <section className='link_container'>
         <Link href={'/search'}><p>검색</p></Link>
       </section>
     </div>
