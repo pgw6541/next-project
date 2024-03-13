61745411f53e5b4c24393abb3064f29fec43260d
pgw6541 <pgw6541@gmail.com>
2024-03-13 17:56:30 +0900

    최상위 클래스 container 삭제

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children, }: { children: React.ReactNode }) {
   return (
-    <html lang="kr" className='container'>
+    <html lang="kr">
       <body className={inter.className}>
         <Providers>
           {children}
