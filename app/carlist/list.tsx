0d6d52957cdbb7a07915e0063eb5118fad44e45b
pgw6541 <pgw6541@gmail.com>
2024-03-15 20:39:52 +0900

    삭제 carlist loading

--- app/carlist/list.tsx
+++ app/carlist/list.tsx
@@ -1,48 +1,13 @@
 'use client'
 
-import { useEffect, useState } from "react";
 import style from './carlist.module.scss'
 import Link from "next/link";
 import Image from "next/image";
 
 import { useAppSelector } from "@/store/hook"
-import * as types from '@/types/types'
-
-interface showDetailCars extends types.Car {
-  showDetail: boolean;
-}
 
 export default function CarList(){
-  const selectBrand = useAppSelector(state => state.selectOption.brand);
-  const selectSeg = useAppSelector(state => state.selectOption.segment);
-  const [loadedImages, setLoadedImages] = useState(0);
-  const [allImagesLoaded, setAllImagesLoaded] = useState(false);
   const chooseCar = useAppSelector( state => state.chooseCar)
-
-  // 카테고리 변경을 감지하고 loadedImages 및 allImagesLoaded를 초기화
-  useEffect(() => {
-    setLoadedImages(0);
-    setAllImagesLoaded(false);
-  }, [selectBrand, selectSeg, chooseCar]);
-
-  // 모든 이미지 로드 되었는지 감지
-  useEffect(()=>{
-    console.log('변경감지')
-    // 선택된 차량이 없거나 || 모든 이미지가 로드되지 않았다면 로딩 상태를 false로 설정합니다.
-    if (chooseCar.length === 0 || loadedImages !== chooseCar.length) {
-      setAllImagesLoaded(false);
-    } else {
-      setAllImagesLoaded(true);
-    }
-  }, [loadedImages, chooseCar.length])
-  
-  // 이미지 로딩 감지 - 완료 핸들러
-  const handleImageLoaded = () => {
-    setLoadedImages((prev) => prev + 1);
-  };
-  
-  console.log(loadedImages)
-  console.log(allImagesLoaded)
   
   return (
     // Car Section
@@ -62,42 +27,20 @@ export default function CarList(){
                   alt={car.name.en}
                   width={360}
                   height={153}
-                  onLoadingComplete={handleImageLoaded}
                 />
               </Link>
               <div className={style.article}>
                 {/* 차량 이름 */}
-                {
-                  allImagesLoaded 
-                  ?
-                  (
-                    <Link className={style.name} href={`/carlist/${car.id}`}>
-                      <p>{car.brand.kr}&nbsp;</p>
-                      <p>{car.name.kr}</p>
-                    </Link>
-                  ) 
-                  :
-                  (
-                    <Link className={style.name} href={`/carlist/${car.id}`}>
-                        <p>Brand&nbsp;</p>
-                        <p>Car_name</p>
-                    </Link>
-                  )
-                }
+                  <Link className={style.name} href={`/carlist/${car.id}`}>
+                    <p>{car.brand.kr}&nbsp;</p>
+                    <p>{car.name.kr}</p>
+                  </Link>
               </div>
             </div>
           </div>
         ))
         :
         <div>검색된 차량이 없습니다.</div>
-      }
-      {/* 이미지 로딩중 표시 */}
-      {
-        !allImagesLoaded && (
-          <div className="loader">
-            <div className="spinner">
-          </div></div>
-        )
       }
     </div>
   )
